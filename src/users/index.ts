904a81c7a607a01c76bf3bb50e980e73f1f3a8b4
Akshatha Laxmi <akshata.lakshmi@gmail.com>
2024-07-16 11:40:01 +0530

    chore: make unnecessary constructor useful

--- src/users/index.ts
+++ src/users/index.ts
@@ -4,11 +4,11 @@ import users from './users-module';
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         console.log(process.env.NODE_PATH)
-        const user = await users.getItem("user", 1);
+        const user = await users.getUser(1);
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: `hello ${user ? user.name : 'user'}!`,
+                message: `hello ${user ? user.username : 'user'}!`,
             }),
         };
     } catch (err) {
--- src/users/users-module.ts
+++ src/users/users-module.ts
@@ -1,9 +1,22 @@
+import { randomUUID } from "crypto";
 import PostgresDB from "../modules/psql";
 
 class Users extends PostgresDB {
-  constructor(connectionString: string) {
-    super(connectionString);
-  }
+    constructor(connectionString: string) {
+        super(connectionString);
+    }
+
+    async addUser(username: string, email: string): Promise<void> {
+      const id = randomUUID();
+        await this.addItem(process.env.USER_TABLE as string, {id, username, email});
+    }
+    
+    async getUser(id: number): Promise<{id: number, username: string, email: string} | null> {
+        return await this.getItem(process.env.USER_TABLE as string, id) as {id: number, username: string, email: string} | null;
+    }
+
+    // Add additional methods or override existing ones as needed
+
 }
 
 const users = new Users(process.env.PSQL_CONNECTION_STRING as string);
