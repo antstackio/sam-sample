1e39366f5433ba2fa914e08b2f6e79f136f9f4e3
Akshatha Laxmi <akshata.lakshmi@gmail.com>
2024-07-16 13:36:36 +0530

    chore: accept suggestions on env vars

--- src/users/users-module.ts
+++ src/users/users-module.ts
@@ -12,13 +12,17 @@ class Users extends PostgresDB {
       const id = randomUUID();
       const userTable = process.env.USER_TABLE;
       if (!userTable) {
-          throw new Error("USER_TABLE environment variable is not set.");
+        throw new Error("USER_TABLE environment variable is not set.");
       }
       await this.addItem(userTable, {id, username, email});
     }
     
     async getUser(id: number): Promise<{id: number, username: string, email: string} | null> {
       // USER_TABLE environment variable contains the name of the table meant for storing user information
+      const userTable = process.env.USER_TABLE;
+      if(!userTable) {
+        throw new Error("USER_TABLE environment variable is not set.");
+      }
       return await this.getItem(process.env.USER_TABLE!, id) as {id: number, username: string, email: string} | null;
     }
 
